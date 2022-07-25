14ffaa0778c32e84af2166bb14331023c53d9e6b
noahyar <noahyar@users.noreply.github.com>
2022-07-25 01:37:19 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter d77f36c78171570c3ddcaedd4b504ed41f7796ad

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import ElementPlus from 'element-plus' // 需要引入到App之前
-import App from './App.vue'
-import 'element-plus/dist/index.css'
-import router from './router/index'
-import store from './store'
-
-createApp(App).use(ElementPlus).use(router).use(store).mount('#app')
